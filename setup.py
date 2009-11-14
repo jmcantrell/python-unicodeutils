53fcb9f6947ba788e3d7ef932ed2147bb67b31a0
Jeremy Cantrell <jmcantrell@gmail.com>
2009-11-14 17:59:09 -0500

    Added some documentation and doctests

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
         name='UnicodeUtils',
-        version='0.3.3',
+        version='0.3.4',
         description='Various small utilities for working with unicode strings.',
         author='Jeremy Cantrell',
         author_email='jmcantrell@gmail.com',
--- unicodeutils/__init__.py
+++ unicodeutils/__init__.py
@@ -2,47 +2,113 @@ import unicodedata, string
 
 ENCODING = 'utf-8'
 
-def encode(value, encoding=None):
-    if not encoding: encoding = ENCODING
-    if value is None: return ''
-    if isinstance(value, str): return value
-    if is_list(value): return list(encode(v) for v in value)
-    if is_dict(value): return dict((encode(k), encode(v)) for k, v in value.items())
-    if not isinstance(value, unicode): value = unicode(value)
+def encode(value, encoding=None): #{{{1
+    """Make sure that the given value is a string or contains strings.
+    >>> encode(u'foo')
+    'foo'
+    >>> encode('foo')
+    'foo'
+    >>> encode([u'foo', [u'bar', u'baz']])
+    ['foo', ['bar', 'baz']]
+    >>> encode({u'foo': [u'bar', u'baz']})
+    {'foo': ['bar', 'baz']}
+    """
+    if not encoding:
+        encoding = ENCODING
+    # None is treated as an empty string
+    if value is None:
+        return ''
+    # If already a string, no need to encode
+    if isinstance(value, str):
+        return value
+    # If list/tuple, recursively encode
+    if is_list(value):
+        return list(encode(v) for v in value)
+    # If dict, recursively encode keys and values
+    if is_dict(value):
+        return dict((encode(k), encode(v)) for k, v in value.items())
+    # Make absolutely sure that value is unicode
+    if not isinstance(value, unicode):
+        value = unicode(value)
     return value.encode(encoding)
 
-def decode(value, encoding=None):
-    if not encoding: encoding = ENCODING
-    if value is None: return u''
-    if isinstance(value, unicode): return value
-    if is_list(value): return list(decode(v) for v in value)
-    if is_dict(value): return dict((decode(k), decode(v)) for k, v in value.items())
-    if not isinstance(value, str): value = str(value)
+def decode(value, encoding=None): #{{{1
+    """Make sure that the given value is unicode or contains unicode.
+    >>> decode('foo')
+    u'foo'
+    >>> decode(u'foo')
+    u'foo'
+    >>> decode(['foo', ['bar', 'baz']])
+    [u'foo', [u'bar', u'baz']]
+    >>> decode({'foo': ['bar', 'baz']})
+    {u'foo': [u'bar', u'baz']}
+    """
+    if not encoding:
+        encoding = ENCODING
+    # None is treated as an empty string
+    if value is None:
+        return u''
+    # If already unicode, no need to decode
+    if isinstance(value, unicode):
+        return value
+    # If list/tuple, recursively decode
+    if is_list(value):
+        return list(decode(v) for v in value)
+    # If dict, recursively decode keys and values
+    if is_dict(value):
+        return dict((decode(k), decode(v)) for k, v in value.items())
+    # Make absolutely sure that value is a string
+    if not isinstance(value, str):
+        value = str(value)
     return unicode(value, encoding)
 
-def strip_non_word(value, replace=None, allowed=''):
-    if replace is None: replace = u'_'
-    pool = string.letters + string.digits + u'-_ ' + allowed
-    return u''.join(map(lambda s: (s in pool and s) or replace, value))
+def strip_non_word(value, replace=None, allowed=None): #{{{1
+    """Remove any non-word characters from a string.
+    >>> strip_non_word('foo.bar?baz')
+    u'foo bar baz'
+    """
+    pool = string.letters + string.digits + u'-_ ' + (allowed or '')
+    return u''.join(map(
+        lambda s: (s in pool and s) or (replace or u' '), value))
 
-def strip_non_ascii(value, replace=None):
-    if replace is None: replace = u'_'
-    return u''.join(map(lambda s: (s <= u'~' and s) or replace, value))
+def strip_non_ascii(value, replace=None): #{{{1
+    """Remove any non-ascii characters from a string.
+    >>> strip_non_ascii(u'a\xe4\xf6\xfcb')
+    u'a   b'
+    """
+    return u''.join(map(
+        lambda s: (s <= u'~' and s) or (replace or u' '), value))
 
-def strip_non_windows(value, replace=None):
-    if replace is None: replace = u'_'
-    return u''.join(map(lambda s: (s not in u':*?;"<>|' and s) or replace, value))
+def strip_non_windows(value, replace=None): #{{{1
+    """Removes any characters that are not allowed in Windows filenames.
+    >>> strip_non_windows('foo:bar?baz')
+    u'foo bar baz'
+    """
+    return u''.join(map(
+        lambda s: (s not in u':*?;"<>|' and s) or (replace or u' '), value))
 
-def normalize(value):
-    return unicode(unicodedata.normalize('NFKD', value).encode('ascii', 'ignore'), encoding='utf-8')
+def normalize(value): #{{{1
+    """Get an ascii approximaton of a string containing special characters.
+    >>> normalize(u'\xe4\xf6\xfc')
+    u'aou'
+    """
+    return unicode(unicodedata.normalize('NFKD', value
+        ).encode('ascii', 'ignore'), encoding='utf-8')
 
-def normalize_symbols(value):
+def normalize_symbols(value): #{{{1
     xlate = {'&': 'and'}
     for symbol, replace in xlate.iteritems():
         value = value.replace(symbol, replace)
     return value
 
-def squeeze(value, s):
+def squeeze(value, s=None): #{{{1
+    """Remove excessive characters.
+    >>> squeeze('   foo   bar  baz  ')
+    'foo bar baz'
+    >>> squeeze('foo__bar___baz__', '_')
+    'foo_bar_baz'
+    """
+    if not s: s = ' '
     value_new = value
     ss = s*2
     while True:
@@ -50,14 +116,19 @@ def squeeze(value, s):
         value_new = value_new.replace(ss, s)
     return value_new.strip(s)
 
-def sanitize(value):
-    return squeeze(normalize_symbols(normalize(strip_non_word(value))).lower().replace(' ', '_'), '_')
+def sanitize(value): #{{{1
+    """Produce a super-safe version of a string.
+    >>> sanitize(u' F\xf6\xf6.bar  baz.  ')
+    u'foo_bar_baz'
+    """
+    return squeeze(normalize_symbols(strip_non_word(normalize(
+        value.replace('.', ' ')))).lower().replace(' ', '_'), '_')
 
-def is_list(value):
+def is_list(value): #{{{1
     return isinstance(value, (list, tuple))
 
-def is_dict(value):
+def is_dict(value): #{{{1
     return isinstance(value, dict)
 
-def listify(value):
+def listify(value): #{{{1
     return value if is_list(value) else [value]
